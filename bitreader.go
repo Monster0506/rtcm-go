82b7a9ccc94a03bde330abff4f88b68fe3790800
Monster0506 <tjraklovits@gmail.com>
2026-08-09 00:28:20 -0400

    ReadUint function

--- bitreader.go
+++ bitreader.go
@@ -8,3 +8,21 @@ type BitReader struct {
 func NewBitReader(data []byte) *BitReader {
 	return &BitReader{data: data}
 }
+
+func (r *BitReader) ReadUint(nbits int) uint64 {
+	var v uint64
+	for nbits > 0 {
+		byteIdx := r.pos >> 3
+		bitOff := r.pos & 7
+		take := 8 - bitOff
+		if take > nbits {
+			take = nbits
+		}
+		shift := 8 - bitOff - take
+		mask := byte((1 << take) - 1)
+		v = v<<take | uint64((r.data[byteIdx]>>shift)&mask)
+		r.pos += take
+		nbits -= take
+	}
+	return v
+}
