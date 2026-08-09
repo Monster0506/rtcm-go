2aef795bc7fae1876f8008b98c8d79b345408099
Monster0506 <tjraklovits@gmail.com>
2026-08-09 00:57:42 -0400

    improvements to readuint

--- bitreader.go
+++ bitreader.go
@@ -1,5 +1,7 @@
 package rtcm
 
+import "fmt"
+
 type BitReader struct {
 	data []byte
 	pos  int
@@ -10,6 +12,9 @@ func NewBitReader(data []byte) *BitReader {
 }
 
 func (r *BitReader) ReadUint(nbits int) uint64 {
+	if nbits < 0 || nbits > 64 {
+		panic(fmt.Sprintf("rtcm: ReadUint: nbits must be between 0 and 64, got %d", nbits))
+	}
 	var v uint64
 	for nbits > 0 {
 		byteIdx := r.pos >> 3
@@ -29,9 +34,6 @@ func (r *BitReader) ReadUint(nbits int) uint64 {
 
 func (r *BitReader) ReadInt(nbits int) int64 {
 	v := r.ReadUint(nbits)
-	if nbits <= 0 || nbits > 64 {
-		return int64(v)
-	}
 	shift := 64 - nbits
 	return int64(v<<shift) >> shift
 }
