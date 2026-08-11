3539c5f26d1d84e0021c9a6195a055f2760702d1
Monster0506 <tjraklovits@gmail.com>
2026-08-11 15:21:43 -0400

    decode msg 1008

--- msg1008.go
+++ msg1008.go
@@ -7,3 +7,14 @@ type Msg1008 struct {
 	SetupID           int
 	AntennaSerial     string
 }
+
+func DecodeMsg1008(payload []byte) (*Msg1008, error) {
+	r := NewBitReader(payload)
+	m := &Msg1008{}
+	m.MessageType = int(r.ReadUint(12))
+	m.StationID = int(r.ReadUint(12))
+	m.AntennaDescriptor = readLengthPrefixedString(r)
+	m.SetupID = int(r.ReadUint(8))
+	m.AntennaSerial = readLengthPrefixedString(r)
+	return m, nil
+}
