bfc2be5b96fc8595e8e223900031c23158b58294
Monster0506 <tjraklovits@gmail.com>
2026-08-12 18:05:42 +0000

    msg1029

---
+++ msg1029.go
@@ -0,0 +1,26 @@
+package rtcm
+
+
+
+
+
+type Msg1029 struct {
+	MessageType     int
+	StationID       int
+	MJDNumber       int
+	SecondsOfDayUTC int
+	NumCharacters   int
+	Text            string
+}
+
+func DecodeMsg1029(payload []byte) (*Msg1029, error) {
+	r := NewBitReader(payload)
+	m := &Msg1029{}
+	m.MessageType = int(r.ReadUint(12))
+	m.StationID = int(r.ReadUint(12))
+	m.MJDNumber = int(r.ReadUint(16))
+	m.SecondsOfDayUTC = int(r.ReadUint(17))
+	m.NumCharacters = int(r.ReadUint(7))
+	m.Text = readLengthPrefixedString(r)
+	return m, nil
+}
