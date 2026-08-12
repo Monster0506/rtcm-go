ae729a08cd92070302a482627bb2514574a5195e
Monster0506 <tjraklovits@gmail.com>
2026-08-12 18:53:42 +0000

    glonass network rtk shared header

---
+++ glonassnetworkrtk.go
@@ -0,0 +1,28 @@
+package rtcm
+
+
+
+
+type GLONASSNetworkRTKHeader struct {
+	MessageType                 int
+	NetworkID                   int
+	SubnetworkID                int
+	GLONASSEpochTimeS           float64
+	MultipleMessageIndicator    bool
+	MasterReferenceStationID    int
+	AuxiliaryReferenceStationID int
+	NumGLONASSDataEntries       int
+}
+
+func decodeGLONASSNetworkRTKHeader(r *BitReader) GLONASSNetworkRTKHeader {
+	var h GLONASSNetworkRTKHeader
+	h.MessageType = int(r.ReadUint(12))
+	h.NetworkID = int(r.ReadUint(8))
+	h.SubnetworkID = int(r.ReadUint(4))
+	h.GLONASSEpochTimeS = float64(r.ReadUint(20)) * 0.1
+	h.MultipleMessageIndicator = r.ReadUint(1) != 0
+	h.MasterReferenceStationID = int(r.ReadUint(12))
+	h.AuxiliaryReferenceStationID = int(r.ReadUint(12))
+	h.NumGLONASSDataEntries = int(r.ReadUint(4))
+	return h
+}
