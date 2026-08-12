0541fbeb2c42eed88d01c4992d4187405440cdc0
Monster0506 <tjraklovits@gmail.com>
2026-08-11 22:44:53 -0400

    msg1011

---
+++ msg1011.go
@@ -0,0 +1,24 @@
+package rtcm
+
+type Msg1011Satellite struct {
+	GLONASSL1Observation
+	GLONASSL2Observation
+}
+
+type Msg1011 struct {
+	GLONASSObservationHeader
+	Satellites []Msg1011Satellite
+}
+
+func DecodeMsg1011(payload []byte) (*Msg1011, error) {
+	r := NewBitReader(payload)
+	h, numSats := decodeGLONASSObservationHeader(r)
+	sats := make([]Msg1011Satellite, numSats)
+	for i := range sats {
+		sats[i] = Msg1011Satellite{
+			GLONASSL1Observation: decodeGLONASSL1Observation(r),
+			GLONASSL2Observation: decodeGLONASSL2Observation(r),
+		}
+	}
+	return &Msg1011{GLONASSObservationHeader: h, Satellites: sats}, nil
+}
